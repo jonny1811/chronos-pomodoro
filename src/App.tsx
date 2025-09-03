55c8b59a9bf7d446889a9b18c4f7792e5c2986e6
Jonny David Lezcano <jonnyda1811@gmail.com>
2025-09-02 23:28:58 -0300

    feat: add child to use page instead of all in a component

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,8 @@
-import { Container } from './components/Container';
-import { Logo } from './components/Logo';
-
 import './styles/theme.css';
 import './styles/global.css';
-import { Menu } from './components/Menu';
-import { CountDown } from './components/CountDown';
-import { DefaultInput } from './components/DefaultInput';
-import { Cycles } from './components/Cycles';
-import { DefaultButton } from './components/DefaultButton';
-import { PlayCircleIcon } from 'lucide-react';
-import { Footer } from './components/Footer';
+
+import { Home } from './pages/Home';
 
 export function App() {
-  return (
-    <>
-      <Container>
-        <Logo />
-      </Container>
-
-      <Container>
-        <Menu />
-      </Container>
-
-      <Container>
-        <CountDown />
-      </Container>
-
-      <Container>
-        <form className='form' action=''>
-          <div className='formRow'>
-            <DefaultInput
-              id='myInput'
-              type='text'
-              labelText='task'
-              placeholder='Escriba algo'
-            />
-          </div>
-
-          <div className='formRow'>
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-
-          <div className='formRow'>
-            <Cycles />
-          </div>
-
-          <div className='formRow'>
-            <DefaultButton icon={<PlayCircleIcon />} />
-          </div>
-        </form>
-      </Container>
-
-      <Container>
-        <Footer />
-      </Container>
-    </>
-  );
+  return <Home />;
 }
---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,17 @@
+import { Container } from '../../components/Container';
+import { MainTemplate } from '../../templates/MainTemplate';
+
+export function Home() {
+  return (
+    <MainTemplate>
+      <Container>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum
+          provident ab nam recusandae laudantium! Odit harum dignissimos
+          exercitationem commodi, soluta cum excepturi quasi mollitia,
+          voluptatum molestias deserunt? Perspiciatis, soluta quos.
+        </p>
+      </Container>
+    </MainTemplate>
+  );
+}
