1c9800fbbdab73f86d51143c12d30fe0dfb1adc8
Jonny David Lezcano <jonnyda1811@gmail.com>
2025-09-03 10:38:15 -0300

    feat: add pages which notFound and aboutPomodoro

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,16 +1,37 @@
 import { Container } from '../../components/Container';
+import { GenericHtml } from '../../components/GenericHtml';
+import { Heading } from '../../components/Heading';
 import { MainTemplate } from '../../templates/MainTemplate';
 
 export function Home() {
   return (
     <MainTemplate>
       <Container>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum
-          provident ab nam recusandae laudantium! Odit harum dignissimos
-          exercitationem commodi, soluta cum excepturi quasi mollitia,
-          voluptatum molestias deserunt? Perspiciatis, soluta quos.
-        </p>
+        <GenericHtml>
+          <Heading>404 - Página no encontrada 🚀</Heading>
+          <p>
+            ¡Ups! Parece que la página que estás intentando acceder no existe.
+            Tal vez se haya ido de vacaciones, haya decidido explorar el
+            universo o se haya perdido en algún lugar entre dos agujeros negros.
+            🌌
+          </p>
+          <p>
+            Pero tranquilo, no estás perdido en el espacio (todavía). Puedes
+            volver de forma segura a la <a href='/'>página principal</a> o{' '}
+            <a href='/history'>al historial</a> — o puedes quedarte por aquí y
+            fingir que encontraste una página secreta a la que solo los
+            exploradores más geniales pueden acceder. 🧭✨
+          </p>
+          <p>
+            Si crees que esta página debería existir (o si quieres charlar sobre
+            viajes en el tiempo y agujeros de gusano), solo tienes que ponerte
+            en contacto. De lo contrario, usa el menú para volver al mundo real.
+          </p>
+          <p>
+            Mientras tanto, aquí tienes una reflexión: "Si una página no existe
+            en internet, ¿habrá existido de verdad?" 🤔💭
+          </p>
+        </GenericHtml>
       </Container>
     </MainTemplate>
   );
